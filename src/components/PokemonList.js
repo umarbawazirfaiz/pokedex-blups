c9630ad489238f9a93a799499ca83f17b34c272c
umarbawazirfaiz <umarbawazirfaiz@gmail.com>
2022-03-04 20:14:07 +0700

    add loader & update readme

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PokemonCard from './PokemonCard';
 import InfiniteScroll from "react-infinite-scroll-component";
 import { getPokemons } from '../services/pokemon';
+import { PulseBubbleLoader } from 'react-loaders-kit';
 
 class PokemonList extends Component{
     constructor(props) {
@@ -46,12 +47,19 @@ class PokemonList extends Component{
     render() {
         const {pokemons} = this.state;
 
+        const loaderProps = {
+            loading: true,
+            size: 35,
+            duration: 1,
+            colors: ['#5e22f0', '#f6b93b']
+        }
+
         return (
         <InfiniteScroll
             dataLength={pokemons.length}
             next={this.fetchMoreData}
             hasMore={true}
-            loader={<h4>Loading....</h4>}
+            loader={<div className="load-more"><PulseBubbleLoader {...loaderProps} /></div>}
             >
             <div className="pokemon-list">{
                 pokemons.map((pokemon, index)=><PokemonCard key={pokemon.name} id={index+1} pokemon={pokemon}/>)
